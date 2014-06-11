4151696b36fff772eeda2e8dfdbe3f945a2262d5
Antoni Bertran <antoni@tresipunt.com>
2014-06-11 10:59:04 +0200

    Table to store the AWS configuration

--- install/CloudTime.sql
+++ install/CloudTime.sql
@@ -11,7 +11,7 @@ CREATE TABLE `course` (
   `aws_configuration` varchar(40) DEFAULT 'default_aws_account',
   PRIMARY KEY (`id`),
   KEY `courseKey` (`courseKey`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE `ec2_ami` (
   `imageId` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE `ec2_instance` (
   `has_elastic_ip` bit DEFAULT 0,
   `elasticIPAaddress` varchar(20) default null; 
   PRIMARY KEY (`instanceId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ec2_instance_user_connection` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,7 +64,7 @@ CREATE TABLE `ec2_instance_user_connection` (
   `session_end` datetime DEFAULT NULL,
   `session_force_close` bit(1) DEFAULT b'0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE `ec2_instance_user_course` (
   `instanceId` varchar(50) NOT NULL DEFAULT '',
@@ -75,7 +75,7 @@ CREATE TABLE `ec2_instance_user_course` (
   `created` datetime DEFAULT NULL,
   `userKeyCreated` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`instanceId`,`id_user`,`id_course`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -88,11 +88,27 @@ CREATE TABLE `user` (
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `userKey` (`userKey`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=186 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE `user_course` (
   `id_user` int(11) NOT NULL DEFAULT '0',
   `id_course` int(11) NOT NULL DEFAULT '0',
   `is_instructor` bit(1) DEFAULT NULL,
   PRIMARY KEY (`id_user`,`id_course`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `aws_configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `aws_key` varchar(70) DEFAULT NULL COMMENT 'mazon Web Services Key. Found in the AWS Security Credentials. You can also pass this value as the first parameter to a service constructor.',
+  `aws_secret` varchar(70) DEFAULT NULL COMMENT 'Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also pass this value as the second parameter to a service constructor.',
+  `aws_account_id` varchar(70) DEFAULT NULL COMMENT 'Amazon Account ID without dashes. Used for identification with Amazon EC2. Found in the AWS Security Credentials',
+  `aws_canonical_id` varchar(70) DEFAULT NULL COMMENT 'Your CanonicalUser ID. Used for setting access control settings in AmazonS3. Found in the AWS Security Credentials.',
+  `aws_canonical_name` varchar(70) not NULL COMMENT 'Your CanonicalUser DisplayName. Used for setting access control settings in AmazonS3. Found in the AWS Security Credentials (i.e. "Welcome, AWS_CANONICAL_NAME").',
+  `aws_mfa_serial` varchar(70) DEFAULT NULL COMMENT '12-digit serial number taken from the Gemalto device used for Multi-Factor Authentication. Ignore this if you\'re not using MFA.',
+  `aws_cloudfront_keypair_id` varchar(70) DEFAULT NULL COMMENT 'Amazon CloudFront key-pair to use for signing private URLs. Found in the AWS Security Credentials. This can be set programmatically with AmazonCloudFront::set_keypair_id()',
+  `aws_cloudfront_private_key_pem` mediumtext DEFAULT NULL COMMENT 'The contents of the *.pem private key that matches with the CloudFront key-pair ID. Found in the AWS Security Credentials. This can be set programmatically with AmazonCloudFront::set_private_key().
+',
+  `aws_enable_extensions` bit DEFAULT 0 COMMENT ' Set the value to true to enable autoloading for classes not prefixed with "Amazon" or "CF". If enabled, load sdk.class.php last to avoid clobbering any other autoloaders.',
+  PRIMARY KEY (`id`),
+  KEY `aws_canonical_name` (`aws_canonical_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
